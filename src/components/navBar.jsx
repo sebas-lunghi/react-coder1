e98c883d29ddebee1bbd486bb26fad896fda3319
sebastian lunghitano <seba_88_@hotmail.com>
2022-09-29 22:45:13 -0300

    preentrega1

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -11,7 +11,7 @@ export default function navBar() {
                 Inicio
             </li>
             <li>
-                Modelos
+                Models
             </li>
             <li>
                 Contacto
